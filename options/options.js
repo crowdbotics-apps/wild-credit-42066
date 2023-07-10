f66e48686878b4225b718326ac6611358ac2c6c6
Crowdbotics <team@crowdbotics.com>
2023-07-10 16:27:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-credit-42066",
+  url: "https://wild-credit-42066.botics.co",
+  api: "https://wild-credit-42066.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
